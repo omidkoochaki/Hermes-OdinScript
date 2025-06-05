fa6e6e82e0945e64e78e30976c498da3696af7b4
robert <robert@mail.internal>
2025-06-05 11:57:36 +0330

    url updated

--- dist/hermesOne.js
+++ dist/hermesOne.js
@@ -1,5 +1,5 @@
 (function () {
-  const ENDPOINT = "https://91.107.181.122:7778/api/v1/project/test";
+  const ENDPOINT = "http://91.107.181.122:7778/api/v1/project/test";
   const SCROLL_THRESHOLD = 0.5; // 50%
   const TIME_ON_PAGE_THRESHOLD = 10 * 1000; // 10 seconds
 
